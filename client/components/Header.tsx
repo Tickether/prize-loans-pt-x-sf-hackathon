b47915ec096a19f3e0b0774cf2ca9b3c22618e50
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-11-18 12:29:23 +0530

    UI improved

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -7,22 +7,32 @@ export function Header() {
   return (
     <>
       <div className="flex justify-center w-full h-full items-center min-h-screen">
-        <div className=" w-full h-full">
-          {" "}
+        <div className="flex flex-col mt-96 mb-96 z-10 w-full h-full">
+          <div className="flex flex-col items-start font-mono   text-9xl font-bold  w-full h-full">
+            <p>WellCome To </p>
+            <p> Pweethy</p>
+          </div>
+
+          <div className=" font-mono items-end  mt-44    text-5xl italic font-bold flex flex-col w-full h-full">
+            <div>
+              <p>obtain Easy loan </p>
+              <p>Againest </p>
+              <p>PoolTogether pweth🪙</p>
+            </div>
+          </div>
+          <div className=" font-mono items-start  mt-44    text-5xl italic font-bold flex flex-col w-full h-full">
+            <div>
+              <p>Pay intrest % by Stream</p>
+              <br />
+              <p>Powerd by Superfluid🌊</p>
+            </div>
+          </div>
+        </div>
+        <div className="fixed   top-0 w-full h-full text-white">
           <SplineViewer
             url="https://prod.spline.design/GH2PsLB0cwKKeEDL/scene.splinecode"
             eventsTarget="global"
           />
-        </div>
-
-        <div className="absolute  w-full h-full text-white">
-          <div className="font-mono  w-full h-full  text-9xl font-bold flex justify-start items-center">
-            <div className="flex-none">
-              {" "}
-              <p>WellCome To </p>
-              <p> Pweethy</p>
-            </div>
-          </div>
         </div>
       </div>
     </>
