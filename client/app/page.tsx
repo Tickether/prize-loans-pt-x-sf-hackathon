a1c7d6a48eb6599247ea970db6f0535cbea28d61
sliponit <77669451+sliponit@users.noreply.github.com>
2023-11-29 21:06:13 +0100

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,6 +3,8 @@ import { SplineViewer } from "@/components/Spline";
 import Navbar from "@/components/Navbar";
 import { Header } from "@/components/Header";
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <main className=" min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
